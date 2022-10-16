0966e6c94f86dba5f327784dff656f81a709c249
Asel Peiris <asel.p@eyepax.com>
2022-10-16 16:00:45 +0530

    create auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,19 @@
+// event object will include body, query params path params etc.
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body)
+  const now = new Date()
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  }
+  // Dyanmic data must never be stored on the global context. 
+
+  // Lambda function is expected to return an object of below format. Note the body must be stringified.
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+    body: JSON.stringify({ auction }),
   };
 }
 
